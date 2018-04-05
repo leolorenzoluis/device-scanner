eeecf354ef62a396f53d5e3fb70dfce1c4e0c23b
Will Johnson <william.c1.johnson@gmail.com>
2018-04-05 13:23:01 -0400

    Add 'remove device' integration test (#111)
    * Add udev-serializer to be used in integration tests
    
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>
    
    * Integrate udev serializer and 'remove device' integration test
    
    The integration tests will be working with a mixture of data. Most of
    the data will be invariant; However, some of the data is variable and
    will change on each run. Multipath devices are a perfect example of
    this, as well as the root and swap partitions, which are mapped
    according to the device mapper. To handle variable data, a serializer
    will be used to transform incoming data. Regular expressions will be
    used to match the keys on the udev object and the variable portions of
    the data will then be transformed such that the data will match the
    corresponding snapshot.
    
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>
    
    * Change serializer from javascript serializer to F# serializer
    
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- IML.IntegrationTest/IntegrationTest.fs
+++ IML.IntegrationTest/IntegrationTest.fs
@@ -6,12 +6,12 @@ module IML.IntegrationTest.IntegrationTest
 
 open Fable.PowerPack
 open Thot
+open Fable.Import
 open Fable.Import.Node.PowerPack
 open IML.CommonLibrary
 open IML.Types.UeventTypes
 open IML.StatefulPromise.StatefulPromise
 open IML.IntegrationTestFramework.IntegrationTestFramework
-
 open Fable.Import.Jest
 open Matchers
 
@@ -22,6 +22,21 @@ let settle () =
 let scannerInfo =
   (fun _ -> pipeToShellCmd "echo '\"Stream\"'" "socat - UNIX-CONNECT:/var/run/device-scanner.sock")
     >>= settle()
+
+let rbScanForDisk (): RollbackState -> RollbackCommandState =
+  rbCmd "for host in `ls /sys/class/scsi_host`; do echo \"- - -\" > /sys/class/scsi_host/$host/scan; done"
+
+let rbSetDeviceState (name:string) (state:string): RollbackState -> RollbackCommandState =
+  rbCmd (sprintf "echo \"%s\" > /sys/block/%s/device/state" state name)
+
+let setDeviceState (name:string) (state:string): State -> JS.Promise<CommandResult<Out, Err>> =
+  cmd (sprintf "echo \"%s\" > /sys/block/%s/device/state" state name)
+
+let deleteDevice (name:string): State -> JS.Promise<CommandResult<Out, Err>> =
+  cmd (sprintf "echo \"1\" > /sys/block/%s/device/delete" name)
+
+let scanForDisk () =
+  cmd "for host in `ls /sys/class/scsi_host`; do echo \"- - -\" > /sys/class/scsi_host/$host/scan; done"
 
 let resultOutput: StatefulResult<State, Out, Err> -> string = function
   | Ok ((Stdout(r), _), _) -> r
@@ -42,4 +57,13 @@ testAsync "stream event" <| fun () ->
     return! scannerInfo
   }
   |> startCommand "Stream Event"
+  |> Promise.map serializeDecodedAndMatch
+
+testAsync "remove a device" <| fun () ->
+  command {
+    do! (setDeviceState "sdc" "offline") >> rollbackError (rbSetDeviceState "sdc" "running") >> ignoreCmd
+    do! (deleteDevice "sdc") >> rollback (rbScanForDisk ()) >> ignoreCmd
+    return! scannerInfo
+  }
+  |> startCommand "removing a device"
   |> Promise.map serializeDecodedAndMatch
