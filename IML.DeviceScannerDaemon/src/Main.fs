fd7f579413cb92da652e45f6efa8c48bdd8fe392
Joe Grund <joe.grund@intel.com>
2018-05-03 09:15:53 -0400

    Add fantomas to device-scanner (#168)
    Now that fantomas (formatter for F# a-la prettier for JS / yapf for
    Python) has dotnet-core support, we can start using it to format our
    code.
    
    Add in fantomas as a dev dep and format a file to show it works.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- IML.DeviceScannerDaemon/src/Main.fs
+++ IML.DeviceScannerDaemon/src/Main.fs
@@ -1,7 +1,6 @@
 // Copyright (c) 2018 Intel Corporation. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
-
 module IML.DeviceScannerDaemon.Server
 
 open Fable.Import.Node
@@ -10,14 +9,13 @@ open Fable.Core.JsInterop
 open Handlers
 open IML.Types.CommandTypes
 
-let serverHandler (c:Net.Socket):unit =
-  c
+let serverHandler (c : Net.Socket) : unit =
+    c
     |> LineDelimited.create()
     |> map Command.decoder
-    |> Readable.onError (fun (e:exn) ->
-      eprintfn "Unable to parse message %s" e.Message
-      c.``end``()
-    )
+    |> Readable.onError (fun (e : exn) -> 
+           eprintfn "Unable to parse message %s" e.Message
+           c.``end``())
     |> tap (Connections.createConn c)
     |> map handler
     |> Readable.onError raise
@@ -25,13 +23,12 @@ let serverHandler (c:Net.Socket):unit =
     |> ignore
 
 let private fd = createEmpty<Net.Fd>
+
 fd.fd <- 3
 
 let opts = createEmpty<Net.CreateServerOptions>
-opts.allowHalfOpen <- Some true
 
-net
-  .createServer(opts, serverHandler)
-  .listen(fd)
-    |> Readable.onError raise
-    |> ignore
+opts.allowHalfOpen <- Some true
+net.createServer(opts, serverHandler).listen(fd)
+|> Readable.onError raise
+|> ignore
