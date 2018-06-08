0f15d182f6e86f822303c2bf064cdafca4fa54a9
Tom Nabarro <tom.nabarro@outlook.com>
2018-06-08 19:48:58 +0100

    start to use elmish
    Signed-off-by: Tom Nabarro <tom.nabarro@outlook.com>

--- IML.DeviceAggregatorDaemon/src/Handlers.fs
+++ IML.DeviceAggregatorDaemon/src/Handlers.fs
@@ -15,91 +15,132 @@ open Query
 open Fable
 open Fable.Import
 
-type DevTree = Map<string, State>
+open Elmish
+open Elmish
+//open Elmish.React
+//open Fable.Helpers.React
 
-type Heartbeats = Map<string, JS.SetTimeoutToken>
+//module Heartbeat =
+type Model =
+    { heartbeats : Map<string, int> } // JS.SetTimeoutToken> }
 
-type AggregatorCommand =
-    | AddHeartbeat of string * (AggregatorCommand -> Heartbeats)
+type Msg =
+    | AddHeartbeat of string
+ ///   * (AggregatorCommand -> Heartbeats)
     | RemoveHeartbeat of string
     | GetTree of Http.ServerResponse
     | UpdateTree of string * string
 
-// type AggregatorState = {
-    // tree: DevTree;
-    // heartbeats: Heartbeats;
-// }
+let init() =
+    { heartbeats = Map.empty }, Cmd.ofMsg (AddHeartbeat "test")
 
-let init() = Map.empty
+//type Model =
+//    { x : int }
 
-let heartbeatTimeout = 30000
+// type Msg =
+    // | Increment
+    // | Decrement
 
-let clearTimeout heartbeats host =
-    Map.tryFind host heartbeats
-    |> Option.map JS.clearTimeout
-    |> ignore
+// let init () =
+    // { x = 0 }, Cmd.ofMsg Increment
 
-let rec handleHeartbeat
-  (state : Heartbeats) (command : AggregatorCommand) : Heartbeats =
-    match command with
-    | AddHeartbeat ((host), (handler)) ->
-          clearTimeout state host
-          let onTimeout() =
-              handler (RemoveHeartbeat host)
-                  |> ignore
-              //heartbeats <- Map.remove host heartbeats
-              //(handler, host)
-          let token = JS.setTimeout onTimeout heartbeatTimeout
-          Map.add host token state
-    | RemoveHeartbeat host ->
-          clearTimeout state host
-          Map.remove host state
+let update msg model =
+    match msg with
+    | AddHeartbeat x -> //when model.x < 3 ->
+        { model with heartbeats = Map.add x 1 model.heartbeats }, Cmd.ofMsg (RemoveHeartbeat "test")
+    | RemoveHeartbeat x -> //when model.x < 3 ->
+        { model with heartbeats = Map.remove x model.heartbeats }, Cmd.ofMsg (AddHeartbeat "test")
     | _ ->
-          state
+        model, Cmd.none
+    // | Increment ->
+        // { model with x = model.x + 1 }, Cmd.ofMsg Decrement
+//
+    // | Decrement when model.x > 0 ->
+        // { model with x = model.x - 1 }, Cmd.ofMsg Decrement
+//
+    // | Decrement ->
+        // { model with x = model.x - 1 }, Cmd.ofMsg Increment
 
-let rec handleTree
-  (state : DevTree) (command : AggregatorCommand) : DevTree =
-    match command with
-    | GetTree response ->
-          runQuery response state Legacy
-          state
-    | UpdateTree ((host), (data)) ->
-          data
-          |> Decode.decodeString State.decoder
-          |> Result.unwrap
-          |> (fun x -> Map.add host x state)
-    | _ ->
-          state
+Program.mkProgram init update (fun model _ -> printf "%A\n" model)
+|> Program.run
 
-let heartbeatReducer = IML.CommonLibrary.scan init handleHeartbeat
-let treeReducer = IML.CommonLibrary.scan init handleTree
-
-let serverHandler (request : Http.IncomingMessage) (response : Http.ServerResponse) =
-    match request.method with
-    | Some "GET" ->
-        treeReducer (GetTree response) |> ignore
-    | Some "POST" ->
-        request
-        |> Stream.reduce "" (fun acc x -> Ok(acc + x.toString ("utf-8")))
-        |> Stream.iter (fun x ->
-               match !!request.headers?("x-ssl-client-name") with
-               | Some "" ->
-                     eprintfn "Aggregator received message but hostname was empty"
-               | Some host ->
-                     match Message.decoder x with
-                     | Ok Message.Heartbeat ->
-                           heartbeatReducer (AddHeartbeat (host, heartbeatReducer)) |> ignore
-                     | Ok(Message.Data y) ->
-                           treeReducer (UpdateTree (host, y)) |> ignore
-                     | Error x ->
-                           eprintfn
-                               "Aggregator received message but message decoding failed (%A)"
-                               x
-               | None ->
-                     eprintfn
-                        "Aggregator received message but x-ssl-client-name header was missing from request"
-        )
-        |> ignore
-    | x ->
-        eprintfn "Aggregator handler got a bad match %A" x
-    response.``end``()
+//type Heartbeats = Map<string, JS.SetTimeoutToken>
+//
+//// type AggregatorState = {
+//    // tree: DevTree;
+//    // heartbeats: Heartbeats;
+//// }
+//
+//let init() = Map.empty
+//
+//let heartbeatTimeout = 30000
+//
+//let clearTimeout heartbeats host =
+//    Map.tryFind host heartbeats
+//    |> Option.map JS.clearTimeout
+//    |> ignore
+//
+//let rec handleHeartbeat
+//  (state : Heartbeats) (command : AggregatorCommand) : Heartbeats =
+//    match command with
+//    | AddHeartbeat ((host), (handler)) ->
+//          clearTimeout state host
+//          let onTimeout() =
+//              handler (RemoveHeartbeat host)
+//                  |> ignore
+//              //heartbeats <- Map.remove host heartbeats
+//              //(handler, host)
+//          let token = JS.setTimeout onTimeout heartbeatTimeout
+//          Map.add host token state
+//    | RemoveHeartbeat host ->
+//          clearTimeout state host
+//          Map.remove host state
+//    | _ ->
+//          state
+//
+//let rec handleTree
+//  (state : DevTree) (command : AggregatorCommand) : DevTree =
+//    match command with
+//    | GetTree response ->
+//          runQuery response state Legacy
+//          state
+//    | UpdateTree ((host), (data)) ->
+//          data
+//          |> Decode.decodeString State.decoder
+//          |> Result.unwrap
+//          |> (fun x -> Map.add host x state)
+//    | _ ->
+//          state
+//
+//let heartbeatReducer = IML.CommonLibrary.scan init handleHeartbeat
+//let treeReducer = IML.CommonLibrary.scan init handleTree
+//
+//let serverHandler (request : Http.IncomingMessage) (response : Http.ServerResponse) =
+//    match request.method with
+//    | Some "GET" ->
+//        treeReducer (GetTree response) |> ignore
+//    | Some "POST" ->
+//        request
+//        |> Stream.reduce "" (fun acc x -> Ok(acc + x.toString ("utf-8")))
+//        |> Stream.iter (fun x ->
+//               match !!request.headers?("x-ssl-client-name") with
+//               | Some "" ->
+//                     eprintfn "Aggregator received message but hostname was empty"
+//               | Some host ->
+//                     match Message.decoder x with
+//                     | Ok Message.Heartbeat ->
+//                           heartbeatReducer (AddHeartbeat (host, heartbeatReducer)) |> ignore
+//                     | Ok(Message.Data y) ->
+//                           treeReducer (UpdateTree (host, y)) |> ignore
+//                     | Error x ->
+//                           eprintfn
+//                               "Aggregator received message but message decoding failed (%A)"
+//                               x
+//               | None ->
+//                     eprintfn
+//                        "Aggregator received message but x-ssl-client-name header was missing from request"
+//        )
+//        |> ignore
+//    | x ->
+//        eprintfn "Aggregator handler got a bad match %A" x
+//    response.``end``()
--- IML.DeviceAggregatorDaemon/test/HandlersTest.fs
+++ IML.DeviceAggregatorDaemon/test/HandlersTest.fs
@@ -13,6 +13,7 @@ open IML.Types.MessageTypes
 open Matchers
 open Handlers
 open IML.Types.Fixtures
+open Elmish
 
 let testServerHost = "localhost"
 let testServerPort = 8181
@@ -32,153 +33,158 @@ let heartbeatPayload =
 
 let private hostname = "foo.com"
 
-testList "Heartbeat"
-    [ let withSetup f () : unit =
-          jest.useFakeTimers() |> ignore
-          let mutable heartbeats = Map.empty
-          f heartbeats
-          jest.clearAllTimers()
-          heartbeats <- Map.empty
-          jest.useRealTimers() |> ignore
-      yield! testFixture withSetup
-                 [ "should register timer",
-                   fun heartbeats ->
-                       heartbeats.ContainsKey hostname === false
-                       let newHeartbeats = handleHeartbeat heartbeats (AddHeartbeat hostname)
-                       newHeartbeats.ContainsKey hostname === true
-                   "should expire timer on timeout",
-                   fun heartbeats ->
-                       heartbeats.ContainsKey hostname === false
-                       let newHeartbeats = handleHeartbeat heartbeats (AddHeartbeat hostname)
-                       newHeartbeats.ContainsKey hostname === true
-                       jest.advanceTimersByTime (heartbeatTimeout + 100)
-                       newHeartbeats.ContainsKey hostname === false ] ]
-                  //  "should restart timer on next heartbeat",
-                  //  fun heartbeats ->
-                      //  heartbeats.ContainsKey hostname === false
-                      //  let newHeartbeats = handleHeartbeat heartbeats (AddHeartbeat hostname)
-                      //  newHeartbeats.ContainsKey hostname === true
-                      //  jest.advanceTimersByTime (heartbeatTimeout - 100)
-                      //  newHeartbeats.ContainsKey hostname === true
-                      //  let newerHeartbeats = handleHeartbeat newHeartbeats (AddHeartbeat hostname)
-                      //  jest.advanceTimersByTime 200
-                      //  newerHeartbeats.ContainsKey hostname === true ] ]
-
-testList "Server"
-    [ let withSetup f (d : Jest.Bindings.DoneStatic) : unit =
-          // bring up server to run test target handler
-          let server = http.createServer(serverHandler).listen(testServerPort)
-
-          // request builder for requests to send to test server
-          let request (method, onData, originHost, lastRequest, headers) =
-              let opts = createEmpty<Https.RequestOptions>
-              opts.hostname <- Some testServerHost
-              opts.port <- Some testServerPort
-              opts.method <- Some method
-              opts.rejectUnauthorized <- Some false
-              match headers with
-              | Some x -> opts.headers <- Some x
-              | None ->
-                  opts.headers <- createObj
-                                      [ "Content-Type" ==> "application/json"
-                                        "x-ssl-client-name" ==> originHost ]
-                                  |> Some
-              http.request (opts,
-                            fun r ->
-                                r
-                                |> reduce ""
-                                       (fun acc x ->
-                                       Ok(acc + x.toString ("utf-8")))
-                                |> iter onData
-                                |> Writable.onFinish (fun () ->
-                                       if lastRequest then
-                                           server.close() |> ignore
-                                           d.``done``())
-                                |> ignore)
-              |> Readable.onError (printfn "error when connecting: %A")
-
-          // send get request and match snapshot on response, terminate test server
-          let getRequest() =
-              request (Http.Methods.Get, toMatchSnapshot, "foo.com", true, None)
-              |> Writable.``end`` None
-          // send post request but don't terminate test server
-          let postRequest host headers =
-              request (Http.Methods.Post, ignore, host, false, headers)
-
-          // send post request with update data and when response is finished, send get to request
-          let postThenGet payload =
-              postRequest "foo.com" None
-              |> Writable.onFinish getRequest
-              |> Writable.``end`` payload
-
-          // send patch request but don't terminate test server
-          let patchRequest host headers =
-              request (Http.Methods.Patch, ignore, host, false, headers)
-          f getRequest postRequest postThenGet patchRequest
-          // heartbeats <- Map.empty
-          // devTree <- Map.empty
-      yield! testFixtureDone withSetup
-                 [ "should receive empty tree in get response without prior update",
-                   fun get _ _ _ ->
-                       expect.assertions 1
-                       get()
-                   "should receive updated tree in get response after post with update",
-                   fun _ _ postThenGet _ ->
-                       expect.assertions 1
-                       postThenGet updatePayload
-                   "should receive empty tree in get response after post with update but no header entry",
-                   fun get post _ _ ->
-                       expect.assertions 1
-                       let headers =
-                           createObj [ "Content-Type" ==> "application/json" ]
-                       post "foo.com" (Some headers)
-                       |> Writable.onFinish get
-                       |> Writable.``end`` updatePayload
-                   "should receive empty tree in get response after post with update but empty hostname",
-                   fun get post _ _ ->
-                       expect.assertions 1
-                       post "" None
-                       |> Writable.onFinish get
-                       |> Writable.``end`` updatePayload
-                   "should receive empty tree in get response after post with heartbeat",
-                   fun _ _ postThenGet _ ->
-                       expect.assertions 1
-                       jest.useFakeTimers() |> ignore
-                       postThenGet heartbeatPayload
-                       jest.runAllTimers()
-                   "should receive empty tree in get response after patch with update",
-                   fun get _ _ patch ->
-                       expect.assertions 1
-                       patch "foo.com" None
-                       |> Writable.onFinish get
-                       |> Writable.``end`` updatePayload
-                   "should receive updated tree in get response after updates from multiple hosts",
-                   fun _ post postThenGet _ ->
-                       expect.assertions 1
-                       post "bar.com" None
-                       |> Writable.onFinish
-                              (fun () -> postThenGet updatePayload)
-                       |> Writable.``end`` updatePayload
-
-                   "should receive tree after updates from multiple hosts with shared pools",
-                   fun _ post postThenGet _ ->
-                       expect.assertions 1
-                       post "bar.com" None
-                       |> Writable.onFinish
-                              (fun () ->
-                              postThenGet
-                                  (updatePayloadWith fixtures.scannerState3))
-                       |> Writable.``end``
-                              (updatePayloadWith fixtures.scannerState2)
-
-                   "should receive tree after updates from multiple hosts with shared datasets",
-                   fun _ post postThenGet _ ->
-                       expect.assertions 1
-                       post "bar.com" None
-                       |> Writable.onFinish
-                              (fun () ->
-                              postThenGet
-                                  (updatePayloadWith
-                                       fixtures.scannerStateDatasets2))
-                       |> Writable.``end``
-                              (updatePayloadWith fixtures.scannerStateDatasets) ] ]
+test "elm"
+<| fun () ->
+    Program.mkProgram init update (fun model _ -> printf "%A\n" model)
+    |> Program.run
+//testList "Heartbeat"
+//    [ let withSetup f () : unit =
+//          jest.useFakeTimers() |> ignore
+//          let mutable heartbeats = Map.empty
+//          f heartbeats
+//          jest.clearAllTimers()
+//          heartbeats <- Map.empty
+//          jest.useRealTimers() |> ignore
+//      yield! testFixture withSetup
+//                 [ "should register timer",
+//                   fun heartbeats ->
+//                       heartbeats.ContainsKey hostname === false
+//                       let newHeartbeats = handleHeartbeat heartbeats (AddHeartbeat hostname)
+//                       newHeartbeats.ContainsKey hostname === true
+//                   "should expire timer on timeout",
+//                   fun heartbeats ->
+//                       heartbeats.ContainsKey hostname === false
+//                       let newHeartbeats = handleHeartbeat heartbeats (AddHeartbeat hostname)
+//                       newHeartbeats.ContainsKey hostname === true
+//                       jest.advanceTimersByTime (heartbeatTimeout + 100)
+//                       newHeartbeats.ContainsKey hostname === false ] ]
+//                  //  "should restart timer on next heartbeat",
+//                  //  fun heartbeats ->
+//                      //  heartbeats.ContainsKey hostname === false
+//                      //  let newHeartbeats = handleHeartbeat heartbeats (AddHeartbeat hostname)
+//                      //  newHeartbeats.ContainsKey hostname === true
+//                      //  jest.advanceTimersByTime (heartbeatTimeout - 100)
+//                      //  newHeartbeats.ContainsKey hostname === true
+//                      //  let newerHeartbeats = handleHeartbeat newHeartbeats (AddHeartbeat hostname)
+//                      //  jest.advanceTimersByTime 200
+//                      //  newerHeartbeats.ContainsKey hostname === true ] ]
+//
+//testList "Server"
+//    [ let withSetup f (d : Jest.Bindings.DoneStatic) : unit =
+//          // bring up server to run test target handler
+//          let server = http.createServer(serverHandler).listen(testServerPort)
+//
+//          // request builder for requests to send to test server
+//          let request (method, onData, originHost, lastRequest, headers) =
+//              let opts = createEmpty<Https.RequestOptions>
+//              opts.hostname <- Some testServerHost
+//              opts.port <- Some testServerPort
+//              opts.method <- Some method
+//              opts.rejectUnauthorized <- Some false
+//              match headers with
+//              | Some x -> opts.headers <- Some x
+//              | None ->
+//                  opts.headers <- createObj
+//                                      [ "Content-Type" ==> "application/json"
+//                                        "x-ssl-client-name" ==> originHost ]
+//                                  |> Some
+//              http.request (opts,
+//                            fun r ->
+//                                r
+//                                |> reduce ""
+//                                       (fun acc x ->
+//                                       Ok(acc + x.toString ("utf-8")))
+//                                |> iter onData
+//                                |> Writable.onFinish (fun () ->
+//                                       if lastRequest then
+//                                           server.close() |> ignore
+//                                           d.``done``())
+//                                |> ignore)
+//              |> Readable.onError (printfn "error when connecting: %A")
+//
+//          // send get request and match snapshot on response, terminate test server
+//          let getRequest() =
+//              request (Http.Methods.Get, toMatchSnapshot, "foo.com", true, None)
+//              |> Writable.``end`` None
+//          // send post request but don't terminate test server
+//          let postRequest host headers =
+//              request (Http.Methods.Post, ignore, host, false, headers)
+//
+//          // send post request with update data and when response is finished, send get to request
+//          let postThenGet payload =
+//              postRequest "foo.com" None
+//              |> Writable.onFinish getRequest
+//              |> Writable.``end`` payload
+//
+//          // send patch request but don't terminate test server
+//          let patchRequest host headers =
+//              request (Http.Methods.Patch, ignore, host, false, headers)
+//          f getRequest postRequest postThenGet patchRequest
+//          // heartbeats <- Map.empty
+//          // devTree <- Map.empty
+//      yield! testFixtureDone withSetup
+//                 [ "should receive empty tree in get response without prior update",
+//                   fun get _ _ _ ->
+//                       expect.assertions 1
+//                       get()
+//                   "should receive updated tree in get response after post with update",
+//                   fun _ _ postThenGet _ ->
+//                       expect.assertions 1
+//                       postThenGet updatePayload
+//                   "should receive empty tree in get response after post with update but no header entry",
+//                   fun get post _ _ ->
+//                       expect.assertions 1
+//                       let headers =
+//                           createObj [ "Content-Type" ==> "application/json" ]
+//                       post "foo.com" (Some headers)
+//                       |> Writable.onFinish get
+//                       |> Writable.``end`` updatePayload
+//                   "should receive empty tree in get response after post with update but empty hostname",
+//                   fun get post _ _ ->
+//                       expect.assertions 1
+//                       post "" None
+//                       |> Writable.onFinish get
+//                       |> Writable.``end`` updatePayload
+//                   "should receive empty tree in get response after post with heartbeat",
+//                   fun _ _ postThenGet _ ->
+//                       expect.assertions 1
+//                       jest.useFakeTimers() |> ignore
+//                       postThenGet heartbeatPayload
+//                       jest.runAllTimers()
+//                   "should receive empty tree in get response after patch with update",
+//                   fun get _ _ patch ->
+//                       expect.assertions 1
+//                       patch "foo.com" None
+//                       |> Writable.onFinish get
+//                       |> Writable.``end`` updatePayload
+//                   "should receive updated tree in get response after updates from multiple hosts",
+//                   fun _ post postThenGet _ ->
+//                       expect.assertions 1
+//                       post "bar.com" None
+//                       |> Writable.onFinish
+//                              (fun () -> postThenGet updatePayload)
+//                       |> Writable.``end`` updatePayload
+//
+//                   "should receive tree after updates from multiple hosts with shared pools",
+//                   fun _ post postThenGet _ ->
+//                       expect.assertions 1
+//                       post "bar.com" None
+//                       |> Writable.onFinish
+//                              (fun () ->
+//                              postThenGet
+//                                  (updatePayloadWith fixtures.scannerState3))
+//                       |> Writable.``end``
+//                              (updatePayloadWith fixtures.scannerState2)
+//
+//                   "should receive tree after updates from multiple hosts with shared datasets",
+//                   fun _ post postThenGet _ ->
+//                       expect.assertions 1
+//                       post "bar.com" None
+//                       |> Writable.onFinish
+//                              (fun () ->
+//                              postThenGet
+//                                  (updatePayloadWith
+//                                       fixtures.scannerStateDatasets2))
+//                       |> Writable.``end``
+//                              (updatePayloadWith fixtures.scannerStateDatasets) ] ]
+//
